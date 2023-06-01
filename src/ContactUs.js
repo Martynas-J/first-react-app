13ddf7e391433ebcd2b1e740f0f670bc1f1dbca8
Martynas-J <Aurimhas@gmail.com>
2023-06-01 09:03:37 +0300

    Contact Us React

---
+++ src/ContactUs.js
@@ -0,0 +1,65 @@
+function ContactUs() {
+    return (
+        <div className="container-xxl">
+        <div className="row align-items-center gx-5 m-0 p-0">
+            <div className="col-md-12 col-xl-6 col-lg-8 order-lg-0 order-md-3">
+                <div className="row g-3">
+                    <h1 className="col-12 py-5 ps-0 fs-2">Find Us</h1>
+                    <div className="col-md-6 col-sm-12">
+                        <div className=" pb-5 shadow p-3 bg-body rounded">
+                            <h2 className="fs-4 fw-bold ">Parduotuvė 1</h2>
+                            <div className="fs-6 fw-bold">Phone: <a className="text-decoration-none fw-normal"
+                                    href="http://">+370666666666</a></div>
+                            <div className="fs-6 fw-bold">Email: <a className="text-decoration-none fw-normal"
+                                    href="http://">shop1@parduotuvė.lt</a></div>
+                            <div className="fs-6 fw-bold">Address: <a className="text-decoration-none fw-normal"
+                                    href="http://">Rotušės aikštė 1, Kaunas</a></div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                        <div className=" pb-5 shadow p-3 bg-body rounded">
+                            <h2 className="fs-4 fw-bold ">Parduotuvė 2</h2>
+                            <div className="fs-6 fw-bold">Phone: <a className="text-decoration-none fw-normal"
+                                    href="http://">+370666666666</a></div>
+                            <div className="fs-6 fw-bold">Email: <a className="text-decoration-none fw-normal"
+                                    href="http://">shop1@parduotuvė.lt</a></div>
+                            <div className="fs-6 fw-bold">Address: <a className="text-decoration-none fw-normal"
+                                    href="http://">Rotušės aikštė 1, Kaunas</a></div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                        <div className=" pb-5 shadow p-3 bg-body rounded">
+                            <h2 className="fs-4 fw-bold ">Parduotuvė 3</h2>
+                            <div className="fs-6 fw-bold">Phone: <a className="text-decoration-none fw-normal"
+                                    href="http://">+370666666666</a></div>
+                            <div className="fs-6 fw-bold">Email: <a className="text-decoration-none fw-normal"
+                                    href="http://">shop1@parduotuvė.lt</a></div>
+                            <div className="fs-6 fw-bold">Address: <a className="text-decoration-none fw-normal"
+                                    href="http://">Rotušės aikštė 1, Kaunas</a></div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                        <div className=" pb-5 shadow p-3 bg-body rounded">
+                            <h2 className="fs-4 fw-bold ">Parduotuvė 4</h2>
+                            <div className="fs-6 fw-bold">Phone: <a className="text-decoration-none fw-normal"
+                                    href="http://">+370666666666</a></div>
+                            <div className="fs-6 fw-bold">Email: <a className="text-decoration-none fw-normal"
+                                    href="http://">shop1@parduotuvė.lt</a></div>
+                            <div className="fs-6 fw-bold">Address: <a className="text-decoration-none fw-normal"
+                                    href="http://">Rotušės aikštė 1, Kaunas</a></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className="col-sm-12 col-xl-6 col-lg-4  ">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2294.3708344038814!2d23.883604316007105!3d54.89641316517835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e722054325aadf%3A0xe3f347e0a7820644!2sRotu%C5%A1%C4%97s%20a.%2C%20Kaunas%2C%20Kauno%20m.%20sav.!5e0!3m2!1slt!2slt!4v1680176053686!5m2!1slt!2slt"
+                    width="100%" height="600px" style={{ border : 0 }} allowFullScreen="" loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+    )
+}
+export default ContactUs
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul className='menu'>
+
+          <li><a className='link' href='#'>Page2</a></li>
+          <li><a className='link' href='#'>Page3</a></li>
+          <li><a className='link' href='#'>About us</a></li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
