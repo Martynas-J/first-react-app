053f98517139f7f6b337c892034f914407de8d13
Martynas-J <Aurimhas@gmail.com>
2023-06-15 12:06:58 +0300

    dogs

--- src/pages/DogsPage/DogsPage.js
+++ src/pages/DogsPage/DogsPage.js
@@ -5,19 +5,14 @@ import { Container } from "react-bootstrap"
 const DogsPage = () => {
 
     const [breeds, setBreeds] = useState({})
+    const [categories, setCategories] = useState([])
     const [category, setCategory] = useState("")
     const [dogPictures, setDogPictures] = useState([])
     const [howMuchPictures, setHowMuchPictures] = useState(1)
-    
 
     useEffect(() => {
-        fetch("https://dog.ceo/api/breeds/list/all")
-            .then(response => response.json())
-            .then(data => setBreeds(data.message))
-    }, [])
-
-    useEffect(() => {
-        fetch(`https://dog.ceo/api/breed/${category}/images/random/${howMuchPictures}`)
+        let isCategory = category ? `/breed/${category}/images/random/${howMuchPictures}` : "/breeds/image/random"
+        fetch(`https://dog.ceo/api${isCategory}`)
             .then(res => res.json())
             .then(data => setDogPictures(data.message))
     }, [category, howMuchPictures])
@@ -25,37 +20,43 @@ const DogsPage = () => {
     const addCategoryHandler = (event) => setCategory(event.target.value)
     const addHowMuchHandler = (event) => setHowMuchPictures(event.target.value)
 
-    const addDogPictureHandler = () => {
-
-    }
-
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/list/all")
+            .then(response => response.json())
+            .then(data => {
+                const breeds = data.message
+                if (Object.keys(breeds).length > 0) {
+                    Object.entries(breeds).map(([mainBreed, subBreeds]) => {
+                        if (subBreeds.length > 0) {
+                            subBreeds.map(subBreed => {
+                                const mainBreedText = mainBreed.at(0).toUpperCase() + mainBreed.slice(1)
+                                const optionText = `${mainBreedText} (${subBreed})`
+                                const optionValue = mainBreed + '/' + subBreed
+                                setCategories(prevState => [...prevState, { optionText, optionValue }])
+                            })
+                        } else {
+                            const optionText = mainBreed.at(0).toUpperCase() + mainBreed.slice(1);
+                            const optionValue = mainBreed;
+                            setCategories(prevState => [...prevState, { optionText, optionValue }])
+                        }
+                    })
+                }
+            })
+    }, [])
     return (
         <Container>
             <h1>DogsPage</h1>
-            {Object.keys(breeds).length > 0 ?
+            {Object.keys(categories).length > 0 ?
                 <>
                     <form>
                         <select onChange={addCategoryHandler} >
                             <option>Breeds</option>
-                            {Object.entries(breeds).map(([mainBreed, subBreeds], index) => {
-                                if (subBreeds.length > 0) {
-                                    return subBreeds.map((subBreed, index )=> {
-                                        const mainBreedText = mainBreed.at(0).toUpperCase() + mainBreed.slice(1)
-                                        const optionText = `${mainBreedText} (${subBreed})`
-                                        const optionValue = mainBreed + '/' + subBreed
-                                        return <option key={index} value={optionValue}>{optionText}</option>
-                                    })
-
-                                } else {
-                                    const optionText = mainBreed.at(0).toUpperCase() + mainBreed.slice(1);
-                                    const optionValue = mainBreed;
-                                    return <option key={index} value={optionValue}>{optionText}</option>
-                                }
-                            })}
+                            {categories.map((category, index) => <option key={index} value={category.optionValue}>{category.optionText}</option>)
+                            }
                         </select>
                         <input type="number" min="1" max="10" value={howMuchPictures} onChange={addHowMuchHandler}></input>
                     </form>
-                    {(category && Array.isArray(dogPictures) && dogPictures.length > 0) ?
+                    {(category && Array.isArray(dogPictures)) ?
                         dogPictures.map((dogPicture, index) => <img key={index} src={dogPicture} alt="Dog" />)
                         : ""}
                 </>
